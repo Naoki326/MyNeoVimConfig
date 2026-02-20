c45ce56ec75607929646f6644cb10c637847c461
naokiz <naokiz@NAOKI>
2026-02-20 15:37:29 +0800

    config smear

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,7 +1,18 @@
 return {
   "sphamba/smear-cursor.nvim",
+  event = "VeryLazy",
   opts = {
     -- 光标拖影效果配置
     -- 可以根据需要调整这些选项
+    stiffness = 0.7,
+    trailing_stiffness = 0.3,
+    trailing_exponent = 1,
+
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    scroll_buffer_space = true,
+    smear_insert_mode = true,
+
+    cursor_color = "#d3cdc3",
   },
 }
