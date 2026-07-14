535f84d85bd7816e2ed17510931943b8022d89dc
zzy <zhiyuan_zheng@roboticplus.com>
2026-07-14 16:15:39 +0800

    feat: 新增 :Chrome 命令打开当前文件 + basic.lua 缩进整理

--- lua/core/basic.lua
+++ lua/core/basic.lua
@@ -19,6 +19,7 @@ vim.opt.smartcase = true
 
 -- 使用系统的剪切板
 vim.opt.clipboard:append("unnamedplus")
+vim.api.nvim_create_user_command('Chrome', '!start chrome "%:p"', {})
 
 -- 设置编码为 utf-8
 vim.o.encoding = 'utf-8'
@@ -29,7 +30,8 @@ vim.o.fileencodings = 'utf-8,gbk,gb18030,gb2312,latin1'
 
 -- 设置默认 shell 为 powershell 7
 vim.o.shell = 'pwsh'
-vim.o.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+vim.o.shellcmdflag =
+'-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
 vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 vim.o.shellquote = ''
@@ -46,17 +48,17 @@ end
 -- SSH 环境下使用 OSC 52 将剪贴板内容转发到本地终端
 -- 要求本地终端支持 OSC 52（Windows Terminal 1.18+、iTerm2、kitty 等）
 if vim.env.SSH_CLIENT ~= nil or vim.env.SSH_TTY ~= nil or vim.env.SSH_CONNECTION ~= nil then
-  vim.g.clipboard = {
-    name = 'OSC 52',
-    copy = {
-      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-    },
-    paste = {
-      -- ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-      -- ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-      ["+"] = my_paste("+"),
-      ["*"] = my_paste("*"),
-    },
-  }
+    vim.g.clipboard = {
+        name = 'OSC 52',
+        copy = {
+            ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+            ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+        },
+        paste = {
+            -- ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+            -- ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+            ["+"] = my_paste("+"),
+            ["*"] = my_paste("*"),
+        },
+    }
 end
